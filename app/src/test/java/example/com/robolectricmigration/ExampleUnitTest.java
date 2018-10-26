1be917e5f270183885f11d29bcf60948a6173f60
Carmen Alvarez <calvarez@libon.com>
2018-10-26 16:20:16 +0200

    Add robolectric 3.8. Add one robolectric test and remove instrumentation tests.

--- app/src/test/java/example/com/robolectricmigration/ExampleUnitTest.java
+++ app/src/test/java/example/com/robolectricmigration/ExampleUnitTest.java
@@ -1,17 +1,19 @@
 package example.com.robolectricmigration;
 
+import android.content.Intent;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.shadows.ShadowApplication;
 
 import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+@RunWith(RobolectricTestRunner.class)
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void deprecatedCodeTest() {
+        Intent nextActivity = ShadowApplication.getInstance().getNextStartedActivity();
+        assertNull(nextActivity);
     }
 }
